30fcf5599d46ec4db8fb116e6a9f778cb6fbc25f
tadurikoushik66 <tadurikoushik66@gmail.com>
2025-01-06 20:31:39 +0530

    message

--- paytm-project/backend/config.js
+++ paytm-project/backend/config.js
@@ -1,4 +1,4 @@
 //backend/config.js
-module.exports = {mongodb+srv://taduri1:bunny1928@cluster0.wjjow.mongodb.net/
-	JWT_SECRET: "your-jwt-secret"
+module.exports = {
+	JWT_SECRET: your-jwt-secret
 }
